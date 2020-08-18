59e2936f549edea4e36e6fb5095092662625ec2f
ShutDownMan <jedson_gabriel@hotmail.com>
2020-08-17 23:48:40 -0300

    First commit

---
+++ app/src/main/java/com/example/libtest/AppConfig.java
@@ -0,0 +1,7 @@
+package com.example.libtest;
+
+public class AppConfig {
+    public static final String STREAM_URL = "rtsp://192.168.4.112:8554/live";
+    public static final String PUBLISHER_USERNAME = "";
+    public static final String PUBLISHER_PASSWORD = "";
+}
---
+++ app/src/main/java/com/example/libtest/MainActivity.java
@@ -0,0 +1,254 @@
+package com.example.libtest;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.SurfaceHolder;
+import android.view.Window;
+import android.view.WindowManager;
+
+import net.majorkernelpanic.streaming.Session;
+import net.majorkernelpanic.streaming.SessionBuilder;
+import net.majorkernelpanic.streaming.audio.AudioQuality;
+import net.majorkernelpanic.streaming.gl.SurfaceView;
+import net.majorkernelpanic.streaming.rtsp.RtspClient;
+import net.majorkernelpanic.streaming.rtsp.RtspServer;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+public class MainActivity extends AppCompatActivity implements RtspClient.Callback,
+        Session.Callback, SurfaceHolder.Callback {
+    // log tag
+    public final static String TAG = MainActivity.class.getSimpleName();
+
+    // surfaceview
+    private static SurfaceView mSurfaceView;
+
+    // Rtsp session
+    private Session mSession;
+    private static RtspClient mClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        // getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_main);
+
+        mSurfaceView = (SurfaceView) findViewById(R.id.surface);
+
+        mSurfaceView.getHolder().addCallback(this);
+
+        verifyStoragePermissions(this);
+
+        // Initialize RTSP client
+        initRtspClient();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        toggleStreaming();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+        toggleStreaming();
+    }
+
+    private void initRtspClient() {
+        // Configures the SessionBuilder
+        mSession = SessionBuilder.getInstance()
+                .setContext(getApplicationContext())
+                .setAudioEncoder(SessionBuilder.AUDIO_NONE)
+                .setAudioQuality(new AudioQuality(8000, 16000))
+                .setVideoEncoder(SessionBuilder.VIDEO_H264)
+                .setSurfaceView(mSurfaceView).setPreviewOrientation(0)
+                .setCallback(this).build();
+
+        // Configures the RTSP client
+        mClient = new RtspClient();
+        mClient.setSession(mSession);
+        mClient.setCallback(this);
+        mClient.setTransportMode(RtspClient.TRANSPORT_UDP);
+        mSurfaceView.setAspectRatioMode(SurfaceView.ASPECT_RATIO_PREVIEW);
+        String ip, port, path;
+
+        // We parse the URI written in the Editext
+        Pattern uri = Pattern.compile("rtsp://(.+):(\\d+)/(.+)");
+        Matcher m = uri.matcher(AppConfig.STREAM_URL);
+        m.find();
+        ip = m.group(1);
+        port = m.group(2);
+        path = m.group(3);
+
+        mClient.setCredentials(AppConfig.PUBLISHER_USERNAME,
+                AppConfig.PUBLISHER_PASSWORD);
+        mClient.setServerAddress(ip, Integer.parseInt(port));
+        mClient.setStreamPath("/" + path);
+
+        mSession.startPreview();
+    }
+
+    private void toggleStreaming() {
+        if (!mClient.isStreaming()) {
+            // Start camera preview
+            mSession.startPreview();
+
+            // Start video stream
+            mClient.startStream();
+        } else {
+            // already streaming, stop streaming
+            // stop camera preview
+            mSession.stopPreview();
+
+            // stop streaming
+            mClient.stopStream();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mClient.release();
+        mSession.release();
+        mSurfaceView.getHolder().removeCallback(this);
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+
+    @Override
+    public void onSessionError(int reason, int streamType, Exception e) {
+        switch (reason) {
+            case Session.ERROR_CAMERA_ALREADY_IN_USE:
+                break;
+            case Session.ERROR_CAMERA_HAS_NO_FLASH:
+                break;
+            case Session.ERROR_INVALID_SURFACE:
+                break;
+            case Session.ERROR_STORAGE_NOT_READY:
+                break;
+            case Session.ERROR_CONFIGURATION_NOT_SUPPORTED:
+                break;
+            case Session.ERROR_OTHER:
+                break;
+        }
+
+        if (e != null) {
+            alertError(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private void alertError(final String msg) {
+        final String error = (msg == null) ? "Unknown error: " : msg;
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage(error).setPositiveButton("Ok",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    @Override
+    public void onRtspUpdate(int message, Exception exception) {
+        switch (message) {
+            case RtspClient.ERROR_CONNECTION_FAILED:
+            case RtspClient.ERROR_WRONG_CREDENTIALS:
+                alertError(exception.getMessage());
+                exception.printStackTrace();
+                break;
+        }
+    }
+
+    @Override
+    public void onPreviewStarted() {
+    }
+
+    @Override
+    public void onSessionConfigured() {
+    }
+
+    @Override
+    public void onSessionStarted() {
+    }
+
+    @Override
+    public void onSessionStopped() {
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+    }
+
+    @Override
+    public void onBitrateUpdate(long bitrate) {
+    }
+
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            READ_EXTERNAL_STORAGE,
+            WRITE_EXTERNAL_STORAGE
+    };
+
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(AppCompatActivity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, WRITE_EXTERNAL_STORAGE);
+
+        if(permission == PackageManager.PERMISSION_GRANTED)
+            Log.d(TAG, "We have storage permission!");
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+
+}
